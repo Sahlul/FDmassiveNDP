fca9c56ddedca1e32cea925ebb8d66d23dbe37a8
irasunat2 <ahmadhendi06@gmail.com>
2022-06-22 08:23:40 -0700

    dari hendi

--- app/src/main/java/com/example/fdmassivendp/home_screen.java
+++ app/src/main/java/com/example/fdmassivendp/home_screen.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class home_screen extends AppCompatActivity {
+
+    ViewFlipper makanan;
+    ViewFlipper minuman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,5 +78,47 @@ public class home_screen extends AppCompatActivity {
         });
 
 
+        int imagesmakanan[] = {R.drawable.gado2,R.drawable.sambal,R.drawable.burger};
+        makanan = findViewById(R.id.v_flipper);
+
+        int imagesminuman[] = {R.drawable.minuman2,R.drawable.dalgonacoffe,R.drawable.minuman};
+        minuman = findViewById(R.id.v_flipper1);
+
+        for (int image: imagesmakanan){
+            flipperimage(image);
+        }
+        for (int image1: imagesminuman){
+            flipperimage1(image1);
+        }
+
+
     }
+
+    private void flipperimage(int image) {
+
+        ImageView imageView = new ImageView (this);
+        imageView.setBackgroundResource(image);
+
+        makanan.addView(imageView);
+        makanan.setFlipInterval(5000);
+        makanan.setAutoStart(true);
+
+        makanan.setInAnimation(this, android.R.anim.slide_in_left);
+        makanan.setOutAnimation(this, android.R.anim.slide_out_right);
+
+    }
+
+    public void flipperimage1 (int image1){
+        ImageView imageView1 = new ImageView (this);
+        imageView1.setBackgroundResource(image1);
+
+        minuman.addView(imageView1);
+        minuman.setFlipInterval(4000);
+        minuman.setAutoStart(true);
+
+        minuman.setInAnimation(this, android.R.anim.slide_in_left);
+        minuman.setOutAnimation(this, android.R.anim.slide_out_right);
+
+    }
+
 }
