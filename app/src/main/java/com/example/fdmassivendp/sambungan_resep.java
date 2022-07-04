ba289e0214971ed96038a3d3388fd531a95e4bfb
unknown <sahlul1000@gmail.com>
2022-07-04 08:11:34 -0700

    melanjutkan desain resep

---
+++ app/src/main/java/com/example/fdmassivendp/sambungan_resep.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sambungan_resep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sambungan_resep);
+    }
+}
---
+++ app/src/main/java/com/example/fdmassivendp/sambungan_resep_1.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sambungan_resep_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sambungan_resep1);
+    }
+}
