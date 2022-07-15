993f2c7f0bfb81b90429119127214c85505b2fcb
sahlul <sahlul1000@gmail.com>
2022-07-15 03:23:55 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/lemetsingkong.java
+++ app/src/main/java/com/example/fdmassivendp/lemetsingkong.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class lemetsingkong extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class lemetsingkong extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lemetsingkong);
+    }
+
+    public void openYoutube(View view) {
     }
 }
