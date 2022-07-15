e49233a89fba0fac3329392a71fca6001f58bd46
sahlul <sahlul1000@gmail.com>
2022-07-15 03:14:27 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/chicken_katsu.java
+++ app/src/main/java/com/example/fdmassivendp/chicken_katsu.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class chicken_katsu extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class chicken_katsu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicken_katsu);
+    }
+
+    public void openYoutube(View view) {
     }
 }
