da074e04d0d26493b32b5ee9846b52ed2a129ada
sahlul <sahlul1000@gmail.com>
2022-07-15 03:02:16 -0700

    dari kris

---
+++ app/src/main/java/com/example/fdmassivendp/sop_tahu.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sop_tahu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sop_tahu);
+    }
+}
