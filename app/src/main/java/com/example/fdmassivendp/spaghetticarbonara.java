7d8c7e78471182559a0191edcb0fafa464c5d656
sahlul <sahlul1000@gmail.com>
2022-07-15 03:19:03 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/spaghetticarbonara.java
+++ app/src/main/java/com/example/fdmassivendp/spaghetticarbonara.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class spaghetticarbonara extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class spaghetticarbonara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spaghetticarbonara);
+    }
+
+    public void openYoutube(View view) {
     }
 }
