f7f7a763eaec87a5a9074729a9d3f54c50ba8ecb
sahlul <sahlul1000@gmail.com>
2022-07-15 03:07:37 -0700

    dari kris

---
+++ app/src/main/java/com/example/fdmassivendp/chicken_katsu.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chicken_katsu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chicken_katsu);
+    }
+}
