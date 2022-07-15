9985589ad9880981daf2fda51b7cd9e84797a5bc
sahlul <sahlul1000@gmail.com>
2022-07-15 03:22:05 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/karedok.java
+++ app/src/main/java/com/example/fdmassivendp/karedok.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class karedok extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class karedok extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_karedok);
+    }
+
+    public void openYoutube(View view) {
     }
 }
