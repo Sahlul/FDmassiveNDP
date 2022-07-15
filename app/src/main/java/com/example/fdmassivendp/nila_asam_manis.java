10a81e99ad87672b61030f966bac1b0dfc9e2d20
sahlul <sahlul1000@gmail.com>
2022-07-15 02:48:26 -0700

    dari kris

---
+++ app/src/main/java/com/example/fdmassivendp/nila_asam_manis.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nila_asam_manis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nila_asam_manis);
+    }
+}
