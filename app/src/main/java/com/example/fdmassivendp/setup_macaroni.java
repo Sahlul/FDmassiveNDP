f8dc98cd30c4fc852e884833c67cab4adab11840
sahlul <sahlul1000@gmail.com>
2022-07-15 03:16:21 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/setup_macaroni.java
+++ app/src/main/java/com/example/fdmassivendp/setup_macaroni.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class setup_macaroni extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class setup_macaroni extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_macaroni);
+    }
+
+    public void openYoutube(View view) {
     }
 }
