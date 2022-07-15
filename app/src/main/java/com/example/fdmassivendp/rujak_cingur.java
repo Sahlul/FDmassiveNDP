036aa17235184dfcdba2b5796fec97e1070d0bde
sahlul <sahlul1000@gmail.com>
2022-07-15 01:07:58 -0700

    dari kris

---
+++ app/src/main/java/com/example/fdmassivendp/rujak_cingur.java
@@ -0,0 +1,14 @@
+package com.example.fdmassivendp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rujak_cingur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rujak_cingur);
+    }
+}
