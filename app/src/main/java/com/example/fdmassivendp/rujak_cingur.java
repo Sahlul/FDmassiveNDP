bbe4dcd3e63b384d26078c35d7d7df05021ff511
sahlul <sahlul1000@gmail.com>
2022-07-15 01:16:41 -0700

    dari kris

--- app/src/main/java/com/example/fdmassivendp/rujak_cingur.java
+++ app/src/main/java/com/example/fdmassivendp/rujak_cingur.java
@@ -3,6 +3,7 @@ package com.example.fdmassivendp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class rujak_cingur extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class rujak_cingur extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rujak_cingur);
+    }
+
+    public void openYoutube(View view) {
     }
 }
